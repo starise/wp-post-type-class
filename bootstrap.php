98a9edd2304c57ccd663af44cd3fc049a500bc30
Andrea Brandi <starise@gmail.com>
2015-12-18 22:05:53 +0100

    Load required class directly

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,6 @@
  * Author URI:  http://stari.se
  */
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-  require __DIR__ . '/vendor/autoload.php';
+if (file_exists(__DIR__ . '/src/PostType.php')) {
+  require __DIR__ . '/src/PostType.php';
 }
